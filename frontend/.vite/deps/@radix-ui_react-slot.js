68c0148e6d69f26e762ebf5d05d308692178451b
Charlie5DH <crmorales5dh@gmail.com>
2025-02-20 09:53:56 -0300

    [Front][Fix] - Fix components path

---
+++ frontend/.vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,12 @@
+import {
+  Root,
+  Slot,
+  Slottable
+} from "./chunk-UWH33HJC.js";
+import "./chunk-YLDSBLSF.js";
+import "./chunk-DC5AMYBS.js";
+export {
+  Root,
+  Slot,
+  Slottable
+};
---
+++ frontend/.vite/deps/@radix-ui_react-toggle.js
@@ -0,0 +1,14 @@
+"use client";
+import {
+  Root,
+  Toggle
+} from "./chunk-WM2EXP4F.js";
+import "./chunk-DM7IVY6Z.js";
+import "./chunk-UWH33HJC.js";
+import "./chunk-TKHB4QMX.js";
+import "./chunk-YLDSBLSF.js";
+import "./chunk-DC5AMYBS.js";
+export {
+  Root,
+  Toggle
+};
